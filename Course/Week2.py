73ca13e387e6d5e8bba6a74736deec5794a12af0
Bachs-JSA05 <bachcaoryan1234@gmail.com>
2024-04-10 04:13:15 +0700

    WEEK2

---
+++ Course/Week2.py
@@ -0,0 +1,66 @@
+#a = 'hello'
+#b= 'hi'
+#print(a+b) dinh nhau
+#print(a,b) cach nhau
+#print(a[1]) vi tri so 1
+#print(a[1:4])  1<=x<4
+#a =16
+#print(f"bach {a} hai")
+#1
+'''
+txt = input("Hello World")
+print(txt [::-1])
+'''
+#: ":"vi tri dau den cuoi "-" bắt đầu cắt
+#3
+''''
+y= input('Nhapten')
+x = y.strip
+'''
+#2
+'''
+a = input('Ten')
+b = input('so lan')
+c= a.count(b)
+print(c)
+'''
+'''
+a = 20//5*2
+print(a)
+'''
+#từ trái sang phải
+#b1
+'''
+a= int(input('nha so:'))
+b= int(input('nha so:'))
+x= (a+b)
+y= (a-b)
+z= (a/b)
+v=(a*b)
+
+print('CỘNG',int(x),float(x),'trừ',int(y),float(y),"chia", int(z),float(z),"Nhan",int(x),float(x) )
+'''
+
+#b2
+'''
+y = int(input(""))
+x = float(y)
+print(y)
+'''
+#b3
+'''
+ c = float(input(""))
+d = int(c)
+print(d)
+'''
+#b4
+'''
+w = float(input("Nhap chieu rong: "))
+l = float(input("Nhap chieu dai: "))
+
+area = w * l
+circumference = (w + l) * 2
+
+print("Dien tich chu nhat: ", float(area), int(area))
+print("Chu vi: ", float(circumference), int(circumference))
+'''
